0988ae5e43f77fa300a51a8e125d23c14bd5a796
= <=>
2025-07-02 09:56:06 -0400

    still trying tofix part 5

--- app/src/main/java/com/project/back_end/controllers/AppointmentController.java
+++ app/src/main/java/com/project/back_end/controllers/AppointmentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Map;
 
